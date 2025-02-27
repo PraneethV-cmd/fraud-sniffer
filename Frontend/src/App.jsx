790f931a4e0c00dd1d70d4fdf4836430e804b08e
Praveen20cent <praveen20cent@gmail.com>
2025-02-27 22:31:54 +0530

    added smooth bg

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./pages/home/home";
-<<<<<<< HEAD
 import HomePage from "./HomePage";
-=======
-import Example from "./components/HomePageComp1";
->>>>>>> f856e834607da2638cf2282e63a6fcbfd50ddf6d
 
 function App() {
 
-  // return (
-  //   <Routes>
-  //     <Route path="/" element={<Home />} />
-  //   </Routes>
-  // )
-
   return (
-<<<<<<< HEAD
     <Routes>
       <Route path="/" element={<HomePage />} />
     </Routes>
-=======
-    <>
-      <Example />
-    </>
->>>>>>> f856e834607da2638cf2282e63a6fcbfd50ddf6d
   )
 }
 
