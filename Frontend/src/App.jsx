e135d96a0f122dc6840aa38ef28bf43462508836
Praveen20cent <praveen20cent@gmail.com>
2025-02-27 22:12:36 +0530

    Homepage backup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home/home";
+import HomePage from "./HomePage";
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   )
 }
--- Frontend/src/HomePage.jsx
+++ Frontend/src/HomePage.jsx
@@ -1,6 +1,16 @@
-export default function HomePage() {
-    return (
-        <>
-        </>
-    )
-}
+// import "./HomePage.css";
+// import React from 'react';
+
+// export default function HomePage() {
+//     return (
+//         <div className="d-flex flex-column justify-content-center w-100 h-100">
+//             <div className="d-flex flex-column justify-content-center align-items-center">
+//                 <div className="btn-group my-5">
+                    
+                    
+//                 </div>
+               
+//             </div>
+//         </div>
+//     );
+// }
